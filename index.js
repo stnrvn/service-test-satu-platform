2db4bc71a4a8ceb65c6cee55cc02be47203e4cdf
stnrvn <sutan@nodeflux.io>
2022-11-16 18:16:54 +0700

    cors allow all

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { connect } = require('./config/mongodb')
 const router = require('./routes/index')
-
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 6000
 
+app.use(cors())
 app.use(express.json())
 
 app.use(express.urlencoded({
